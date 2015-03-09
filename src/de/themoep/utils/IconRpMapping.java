3cb1d586e04da885c1a110c1ec9291d5808d5557
Max Lee <Phoenix616@users.noreply.github.com>
2015-03-09 23:38:10 +0100

    Removed unused imports

--- src/de/themoep/utils/IconRpMapping.java
+++ src/de/themoep/utils/IconRpMapping.java
@@ -1,15 +1,11 @@
 package de.themoep.utils;
 
 import org.bukkit.ChatColor;
-import org.bukkit.Color;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.LeatherArmorMeta;
-import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.potion.PotionEffectType;
-import org.bukkit.potion.PotionEffectTypeWrapper;
 
 import java.util.ArrayList;
 import java.util.List;
