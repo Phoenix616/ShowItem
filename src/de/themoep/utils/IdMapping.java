a54d453ed28359bd5f57a93bdf6142823b762bea
Max Lee <Phoenix616@users.noreply.github.com>
2015-03-17 18:17:22 +0100

    Made utils MPL v2.0

--- src/de/themoep/utils/IdMapping.java
+++ src/de/themoep/utils/IdMapping.java
@@ -1,4 +1,19 @@
 package de.themoep.utils;
+/*
+* Copyright (C) 2015 Max Lee (https://github.com/Phoenix616)
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the Mozilla Public License as published by
+* the Mozilla Foundation, version 2.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* Mozilla Public License v2.0 for more details.
+*
+* You should have received a copy of the Mozilla Public License v2.0
+* along with this program. If not, see <http://mozilla.org/MPL/2.0/>.
+*/
 
 import org.apache.commons.lang.WordUtils;
 import org.bukkit.Material;
@@ -8,9 +23,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Created by Phoenix616 on 03.03.2015.
- */
 public class IdMapping {
     
     Map<Material,String> mcidmap = new HashMap<Material, String>();
