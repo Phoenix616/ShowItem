649c4dd39565864af38c7d2b08ab1ff8bce43d7c
Max Lee <Phoenix616@users.noreply.github.com>
2015-03-17 18:22:10 +0100

    Added titles. Also formatting is important!

--- src/de/themoep/utils/IconRpMapping.java
+++ src/de/themoep/utils/IconRpMapping.java
@@ -1,20 +1,22 @@
 package de.themoep.utils;
 
 /*
-* Copyright (C) 2015 Max Lee (https://github.com/Phoenix616)
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the Mozilla Public License as published by
-* the Mozilla Foundation, version 2.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* Mozilla Public License v2.0 for more details.
-*
-* You should have received a copy of the Mozilla Public License v2.0
-* along with this program. If not, see <http://mozilla.org/MPL/2.0/>.
-*/
+ * IconRpMapping utils for mapping of Bukkit materials to WolfieMario's
+ * Custom Text Icons Resourcepack (http://imgur.com/a/oHvbX)
+ * Copyright (C) 2015 Max Lee (https://github.com/Phoenix616)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Mozilla Public License as published by
+ * the Mozilla Foundation, version 2.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Mozilla Public License v2.0 for more details.
+ *
+ * You should have received a copy of the Mozilla Public License v2.0
+ * along with this program. If not, see <http://mozilla.org/MPL/2.0/>.
+ */
 
 import org.bukkit.*;
 import org.bukkit.inventory.ItemStack;
